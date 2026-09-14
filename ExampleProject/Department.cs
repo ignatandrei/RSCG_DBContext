195c9c64064c29eae29bea51c6bb1e4ebc28d2ca
Andrei Ignat <ignatandrei@yahoo.com>
2026-09-14 14:20:33 +0300

    Add example project and all-DbSet check
    Add a sample EF Core project to exercise the source generator and demonstrate the generated `Exists_*` APIs, including a new `Exists_AllDBSets()` helper that verifies every DbSet. The generator is also refactored into partial support files and now emits the embedded attribute definition with an internal attribute type for cleaner generated output.

---
+++ ExampleProject/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleDatabase;
+
+public partial class Department
+{
+    public Department()
+    {
+        Employee = new HashSet<Employee>();
+        Name = string.Empty;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<Employee> Employee { get; set; }
+}
